8c6d81ca20afdd85cb8631a68cc5f35e3e14962c
Joel Rudsberg <rudsberg@live.se>
2020-02-12 15:33:02 +0100

    more refactoring, labb done

--- CCHAT/server.erl
+++ CCHAT/server.erl
@@ -10,7 +10,7 @@
 % Starts genserver instance with the ServerAtom process and initializes the server
 % with an empty state. 
 start(ServerAtom) ->
-    genserver:start(ServerAtom, #server_state{}, fun server:handle/2).
+    genserver:start(ServerAtom, emptyState(), fun server:handle/2).
 
 % If channel does not exist before, a new channel will get created and client
 % will join this newly created channel. If channel exist and client is not 
@@ -43,21 +43,27 @@ handle(St = #server_state{nicks = Nicks}, {nick, OldNick, NewNick}) ->
         {reply, nick, NewState}
     end;
 
-
-handle(St = #server_state {nicks = Nicks}, {add_user, User}) ->
-    NewState = St#server_state{nicks = [User | Nicks]},
-    {reply,add_user,NewState};
-
-handle(St = #server_state {channels = Channels},stop_channels) ->  
+% Attempts to stop all channels. If any channel is not correctly shut down (ok not returned)
+% an error will be returned and the new server state consist of the channels that was not 
+% shutdown. If all succeeds to shut down, no channels will remain in the state.
+handle(St = #server_state {channels = Channels}, stop_channels) ->  
     ShutDownChannelsWithStatus = lists:zip([genserver:stop(list_to_atom(Ch)) || Ch <- Channels], Channels),
     NotShutdown = [Ch || {Status, Ch} <- ShutDownChannelsWithStatus, Status /= ok],
 
     if length(NotShutdown) == 0 ->    
-        {reply,stop_channels,St};
+        {reply, stop_channels, St#server_state{channels = []}};
     true ->           
         {error, {server_not_reached,"Server not reached."}, St#server_state{channels = NotShutdown}}
     end.
 
+% Attempts to stop all channels and then stop the server process.
+stop(ServerAtom) ->
+    case catch (genserver:request(ServerAtom,stop_channels)) of
+        stop_channels -> genserver:stop(ServerAtom);
+        _             -> {reply, {error, server_not_reached, "Timed out."}, emptyState()}
+    end.
+
+% Helpers
 addNickIfNotAddedBefore(St = #server_state{nicks = Nicks}, Nick) ->
     UserInServer = lists:member(Nick, Nicks),
     if UserInServer ->
@@ -66,10 +72,5 @@ addNickIfNotAddedBefore(St = #server_state{nicks = Nicks}, Nick) ->
         St#server_state{nicks = [Nick | Nicks]}
     end.
 
-
-stop(ServerAtom) ->
-    case  (catch (genserver:request(ServerAtom,stop_channels))) of
-        stop_channels -> genserver:stop(ServerAtom);
-        _          -> {reply,{error,server_not_reached,"Timed out."},#server_state{}}
-    end.
-
+emptyState() ->
+    #server_state{}.
